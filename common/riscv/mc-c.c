a201f4f467444ddc0b4ad2f8f870e58b414b521f
xxxxfast <xing.ziyue@zte.com.cn>
2026-02-05 12:05:29 +0800

    Add RVV optimization for plane_copy

--- common/riscv/mc-c.c
+++ common/riscv/mc-c.c
@@ -101,7 +101,9 @@ void x264_mc_copy_w4_rvv ( pixel *, intptr_t, pixel *, intptr_t, int );
 void x264_mc_copy_w8_rvv ( pixel *, intptr_t, pixel *, intptr_t, int );
 #define x264_mc_copy_w16_rvv x264_template(mc_copy_w16_rvv)
 void x264_mc_copy_w16_rvv( pixel *, intptr_t, pixel *, intptr_t, int );
-
+#define x264_plane_copy_core_rvv x264_template(plane_copy_core_rvv)
+void x264_plane_copy_core_rvv( pixel *dst, intptr_t i_dst,
+                               pixel *src, intptr_t i_src, int w, int h );
 static void (* const pixel_avg_wtab_rvv[6])( pixel *, intptr_t, pixel *, intptr_t, pixel *, int ) =
 {
     NULL,
@@ -211,7 +213,7 @@ static pixel *get_ref_rvv( pixel *dst,   intptr_t *i_dst_stride,
         return src1;
     }
 }
-
+PLANE_COPY(16, rvv)
 void x264_mc_init_rvv( uint32_t cpu, x264_mc_functions_t *pf )
 {
 #if !HIGH_BIT_DEPTH
@@ -239,7 +241,7 @@ void x264_mc_init_rvv( uint32_t cpu, x264_mc_functions_t *pf )
 
         pf->mc_luma = mc_luma_rvv;
         pf->get_ref = get_ref_rvv;
-
+        pf->plane_copy = plane_copy_rvv;
         pf->mc_chroma = x264_mc_chroma_rvv;
     }
 #endif // !HIGH_BIT_DEPTH
